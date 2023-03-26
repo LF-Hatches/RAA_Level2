8fd542ff41f972aeac0b440a7600892ee9c7ddf3
lfarrell <lfarrell@iconarch.com>
2023-03-26 16:53:24 -0400

    27:35 Timestamp https://www.archsmarter.com/products/revit-add-in-academy-level-2/categories/2151986310/posts/21642313774
    Forms Created - Material Button, Renumberer, Excel, CSV.
    Also upgraded to 2023.

--- App.cs
+++ App.cs
@@ -31,14 +31,18 @@ namespace RAA_Level2
             RibbonPanel panel = Utils.CreateRibbonPanel(app, "RAA", "Revit Tools");
 
             // 3. Create button data instances
-            ButtonDataClass myButtonData = new ButtonDataClass("btn_RAA_Module1", "CSV Setup", Command.GetMethod(), Properties.Resources.Excel_32, Properties.Resources.Create_16, "This is a tooltip");
-            ButtonDataClass myButtonData2 = new ButtonDataClass("btn_RAA_Module1Bonus", "Excel Setup", Command2.GetMethod(), Properties.Resources.Excel_32, Properties.Resources.Create_16, "This is a tooltip");
-            ButtonDataClass myButtonData3 = new ButtonDataClass("btn_RAA_MaterialMaker", "Make Pattern", CommandMaterial.GetMethod(), Properties.Resources.Create_32, Properties.Resources.Create_16, "This is a tooltip");
+            ButtonDataClass myButtonData = new ButtonDataClass("btn_RAA_Module1", "CSV Setup", Command.GetMethod(), Properties.Resources.Excel_32, Properties.Resources.Create_16, "Make Levels from a List");
+            ButtonDataClass myButtonData2 = new ButtonDataClass("btn_RAA_Module1Bonus", "Excel Setup", Command2.GetMethod(), Properties.Resources.Excel_32, Properties.Resources.Create_16, "Make Levels from a List");
+            ButtonDataClass myButtonData3 = new ButtonDataClass("btn_RAA_MaterialMaker", "Make Pattern", CommandMaterial.GetMethod(), Properties.Resources.Create_32, Properties.Resources.Create_16, "Create a Pattern from Lines");
+            ButtonDataClass myButtonData4 = new ButtonDataClass("btn_RAA_Renumbering", "Renumber Views", CommandRenumber.GetMethod(), Properties.Resources.Create_32, Properties.Resources.Create_16, "Renumber Views on a Sheet");
+            ButtonDataClass myButtonData5 = new ButtonDataClass("btn_RAA_SheetMaker", "Make Sheets", CommandSheetMaker.GetMethod(), Properties.Resources.Create_32, Properties.Resources.Create_16, "Make Sheets from a Dialog Box");
 
             // 4. Create buttons
             PushButton myButton = panel.AddItem(myButtonData.Data) as PushButton;
-            PushButton myButton2 = panel.AddItem(myButtonData2.Data) as PushButton; 
+            PushButton myButton2 = panel.AddItem(myButtonData2.Data) as PushButton;
             PushButton myButton3 = panel.AddItem(myButtonData3.Data) as PushButton;
+            PushButton myButton4 = panel.AddItem(myButtonData4.Data) as PushButton;
+            PushButton myButton5 = panel.AddItem(myButtonData5.Data) as PushButton;
 
             return Result.Succeeded;
         }
